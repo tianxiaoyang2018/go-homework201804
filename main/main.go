1466ff4f3315ae5c8702ca271746260c667f9df8
tianxiaoyang2018 <2389679202@qq.com>
2018-09-17 21:05:13 +0800

    ‘1’1

--- main/main.go
+++ main/main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"time"
-	"fmt"
+"fmt"
+	"unicode/utf8"
 )
 
 func main() {
-	var timestamp int64 = 123
-	var time time.Time = time.Unix(timestamp, 0)
-	t := &time
-	fmt.Println(*t)
+	fmt.Println(utf8.RuneCountInString(""))
+
 }
