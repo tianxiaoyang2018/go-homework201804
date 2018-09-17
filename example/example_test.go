8f8f50c57c7bd8fe066649093e7222015a8c49f2
root <root@tianxiaoyang.dev.p1staff.com>
2018-09-17 18:58:33 +0800

    ‘1’

---
+++ example/example_test.go
@@ -0,0 +1,12 @@
+package example_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/tianxiaoyang2018/go-test/example"
+)
+
+var _ = Describe("Example", func() {
+
+})
