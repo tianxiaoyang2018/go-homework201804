ed285f0c6c648948e871adaf26801d290aeb0921
root <root@tianxiaoyang.dev.p1staff.com>
2018-09-18 15:24:22 +0800

    ‘1’

--- main/main_test.go
+++ main/main_test.go
@@ -2,11 +2,11 @@ package main_test
 
 import (
 	. "github.com/onsi/ginkgo"
-
+	"github.com/tianxiaoyang2018/go-test/main"
 )
 
 var _ = Describe("Main", func() {
 	It("第一个测试用例", func() {
-		Test()
+		main.Test()
 	})
 })
